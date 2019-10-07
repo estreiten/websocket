5ff7d1321f22c6df601692089cd36581190e8e5d
asadito <streitenberger.ezequiel@gmail.com>
2019-10-07 12:20:55 -0300

    feat: hide config and create sample

---
+++ config.sample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  filePath: 'd:\\websocket\\reboot.txt',
+  port: 3000,
+  seconds: 60,
+  https: {
+    keyFile: 'C:\\server_key.pem',
+    certFile: 'C:\\server_cert.pem'
+  }
+}
